1864d087c3ebe88bac03d7ee54b6a67c29712161
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-03-23 14:34:54 +0400

    While Example Done

---
+++ src/main/java/whileExample/WhileExample.java
@@ -0,0 +1,15 @@
+package whileExample;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        do {
+            System.out.println("This is do operation");
+            i++;
+        }
+        while (i < 10);
+    }
+}
+
